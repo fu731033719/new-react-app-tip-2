1bc6a530cee273fa449e397994357f1872d1c66d
fu731033719 <15706804793@163.com>
2019-02-02 18:28:37 +0800

    redux 对输入值以及列表项更新操作

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,18 +8,43 @@ import store from './store/index';
 class TodoList extends Component {
     constructor (props) {
         super(props);
-        this.status = store.getState();
+        this.state = store.getState();
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleAddItem = this.handleAddItem.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        store.subscribe(this.handleStoreChange);
+    }
+    handleInputChange (e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value,
+        }
+        store.dispatch(action);
+    }
+    handleAddItem () {
+        const action = {
+            type: 'add_item',
+            value: [...this.state.list, this.state.inputValue]
+        }
+        store.dispatch(action);
+    }
+    handleStoreChange () {
+        this.setState(store.getState());
     }
     render () {
         return (
             <div style={{marginTop: '10px'}}>
-                <Input placeholder = "Todo info" style={{width: '300px', marginRight: '10px'}} value={this.status.inputValue}/>
-                <Button type="primary">提交</Button>
+                <Input 
+                placeholder = "Todo info" style={{width: '300px', marginRight: '10px'}} 
+                value={this.state.inputValue}
+                onChange={this.handleInputChange}
+                />
+                <Button type="primary" onClick={this.handleAddItem}>提交</Button>
                 <List
                     header={<div>Header</div>}
                     footer={<div>Footer</div>}
                     bordered
-                    dataSource={this.status.list}
+                    dataSource={this.state.list}
                     renderItem={item => (<List.Item>{item}</List.Item>)}
                     style={{marginTop: '10px'}}
                 />
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,5 +10,16 @@ const defaultstat = {
 };
 
 export default (state = defaultstat, action) => {
+    if (action.type === 'change_input_value' || action.type === 'input_clear') {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+    }
+    if (action.type === 'add_item') {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list = action.value;
+        newState.inputValue = '';
+        return newState;
+    }
     return state;
 }
